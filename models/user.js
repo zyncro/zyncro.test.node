c53cab6d766c74783585d59e1c35918bf1b6f52f
Eduardo <eduugr@gmail.com>
2015-09-09 18:08:41 +0200

    Comienzo a armar modelos con funciones DAO + Promises Bluebird

--- models/user.js
+++ models/user.js
@@ -12,6 +12,49 @@ var UserSchema = new mongoose.Schema({
     "followers" : [String]
 });
 
+Promise = require('bluebird');
+
+
+UserSchema.statics.findByUsername = function(username, callback) {
+    return this.findOne({ username : username }, callback);
+};	
+
+
+/* 	Creates a new User
+**
+**		data		Array { username : 'username', displayName : 'displayName' }
+**      returns		The new created user @ Promise
+*/ 
+UserSchema.statics.createSignUp = function( data ) {	
+	var Self = this;
+
+	return new Promise(function(resolve, reject) {
+
+		// Verificamos que el nombr de usuario no exista aún
+		Self.findByUsername( data.username, function(err, user) {
+
+			// If the username is in use we should reject the request
+			if (user) {
+	        	reject( new Error("The username '"+ data.username +"' is already in use.") );
+	        }
+
+	        // Prepare the new user data
+	        var user = new Self;
+	        user.username = data.username;
+	        user.displayName = data.displayName;
+
+	        // Save the new user and resolve if ok
+	        user.save(function(err, user) {
+	        	if (err) {
+	        		reject(err);
+	        	}
+
+	        	resolve(user);
+	        });
+		});
+	});
+};
+
 
 // Return the model
 module.exports = mongoose.model('User', UserSchema);
